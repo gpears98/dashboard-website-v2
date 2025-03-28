c8829375479568f0586d4b800ea07979fd29bd51
Garrett <111312204+gpears98@users.noreply.github.com>
2025-03-28 17:04:43 -0500

    added sidebar

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,228 @@
+'use client'
+import type { Metadata } from "next";
+import { Avatar } from '@/app/components/avatar'
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+} from '@/app/components/dropdown'
+import { Navbar, NavbarItem, NavbarSection, NavbarSpacer } from '@/app/components/navbar'
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarHeading,
+  SidebarItem,
+  SidebarLabel,
+  SidebarSection,
+  SidebarSpacer,
+} from '@/app/components/sidebar'
+import { SidebarLayout } from '@/app/components/sidebar-layout'
+import {
+    ArrowRightStartOnRectangleIcon,
+    ChevronDownIcon,
+    ChevronUpIcon,
+    MoonIcon,
+    HomeIcon,
+    InboxIcon,
+    LightBulbIcon,
+    MagnifyingGlassIcon,
+    CalendarIcon,
+    PlusIcon,
+    QuestionMarkCircleIcon,
+    ShieldCheckIcon,
+    SparklesIcon,
+    BookOpenIcon,
+    SunIcon,
+    UserIcon,
+    Cog8ToothIcon,
+    ChartBarSquareIcon,
+  } from '@heroicons/react/24/outline'
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarLayout
+      navbar={
+        <Navbar>
+          <NavbarSpacer />
+          <NavbarSection>
+            <NavbarItem href="/search" aria-label="Search">
+              <MagnifyingGlassIcon />
+            </NavbarItem>
+            <NavbarItem href="/https://mail.google.com/mail/u/0/#inbox" aria-label="Inbox" target='_blank' rel='noopener noreferrer'>
+              <InboxIcon />
+            </NavbarItem>
+            <Dropdown>
+              <DropdownButton as={NavbarItem}>
+                <Avatar src="/profile-photo.jpg" square />
+              </DropdownButton>
+              <DropdownMenu className="min-w-64" anchor="bottom end">
+                <DropdownItem href="/my-profile">
+                  <UserIcon />
+                  <DropdownLabel>My profile</DropdownLabel>
+                </DropdownItem>
+                <DropdownItem href="/settings">
+                  <Cog8ToothIcon />
+                  <DropdownLabel>Settings</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem href="/privacy-policy">
+                  <ShieldCheckIcon />
+                  <DropdownLabel>Privacy policy</DropdownLabel>
+                </DropdownItem>
+                <DropdownItem href="/share-feedback">
+                  <LightBulbIcon />
+                  <DropdownLabel>Share feedback</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem href="/logout">
+                  <ArrowRightStartOnRectangleIcon />
+                  <DropdownLabel>Sign out</DropdownLabel>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarSection>
+        </Navbar>
+      }
+      sidebar={
+        <Sidebar>
+          <SidebarHeader>
+            <Dropdown>
+              <DropdownButton as={SidebarItem} className="lg:mb-2.5">
+                <Avatar src="/tailwind-logo.svg" />
+                <SidebarLabel>Tailwind Labs</SidebarLabel>
+                <ChevronDownIcon />
+              </DropdownButton>
+              <DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
+                <DropdownItem href="/teams/1/settings">
+                  <Cog8ToothIcon />
+                  <DropdownLabel>Settings</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem href="/teams/1">
+                  <Avatar slot="icon" src="/tailwind-logo.svg" />
+                  <DropdownLabel>Tailwind Labs</DropdownLabel>
+                </DropdownItem>
+                <DropdownItem href="/teams/2">
+                  <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
+                  <DropdownLabel>Workcation</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem href="/teams/create">
+                  <PlusIcon />
+                  <DropdownLabel>New Location&hellip;</DropdownLabel>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+            <SidebarSection className="max-lg:hidden">
+              <SidebarItem href="/search">
+                <MagnifyingGlassIcon />
+                <SidebarLabel>Search</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="https://mail.google.com/mail/u/0/#inboxinbox/" target='_blank' rel='noopener norefrerrer'>
+                <InboxIcon />
+                <SidebarLabel>Inbox</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+          </SidebarHeader>
+          <SidebarBody>
+            <SidebarSection>
+              <SidebarItem href="/dashboard">
+                <HomeIcon />
+                <SidebarLabel>Home</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/events">
+                <BookOpenIcon />
+                <SidebarLabel>Reservations</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/orders">
+                <SunIcon />
+                <SidebarLabel>Opening</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/settings">
+                <MoonIcon />
+                <SidebarLabel>Closing</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/broadcasts">
+                <CalendarIcon />
+                <SidebarLabel>Scheduling</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/broadcasts">
+                <ChartBarSquareIcon />
+                <SidebarLabel>Reports</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+            <SidebarSection className="max-lg:hidden">
+                <SidebarHeading>Today’s Important Games</SidebarHeading>
+                <SidebarItem href="/sports/1">Pickup Soccer - 9am</SidebarItem>
+                <SidebarItem href="/sports/2">Basketball League - 1pm</SidebarItem>
+            </SidebarSection>
+            <SidebarSection className="max-lg:hidden">
+                <SidebarHeading>Today’s Reservations</SidebarHeading>
+                <SidebarItem href="/reservations/1">Studio A - 10am</SidebarItem>
+                <SidebarItem href="/reservations/2">Conference Room - 3pm</SidebarItem>
+            </SidebarSection>
+            <SidebarSpacer />
+            <SidebarSection>
+              <SidebarItem href="/support">
+                <QuestionMarkCircleIcon />
+                <SidebarLabel>Support</SidebarLabel>
+              </SidebarItem>
+              <SidebarItem href="/changelog">
+                <SparklesIcon />
+                <SidebarLabel>Changelog</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+          </SidebarBody>
+          <SidebarFooter className="max-lg:hidden">
+            <Dropdown>
+              <DropdownButton as={SidebarItem}>
+                <span className="flex min-w-0 items-center gap-3">
+                  <Avatar src="/profile-photo.jpg" className="size-10" square alt="" />
+                  <span className="min-w-0">
+                    <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">Erica</span>
+                    <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+                      erica@example.com
+                    </span>
+                  </span>
+                </span>
+                <ChevronUpIcon />
+              </DropdownButton>
+              <DropdownMenu className="min-w-64" anchor="top start">
+                <DropdownItem href="/my-profile">
+                  <UserIcon />
+                  <DropdownLabel>My profile</DropdownLabel>
+                </DropdownItem>
+                <DropdownItem href="/settings">
+                  <Cog8ToothIcon />
+                  <DropdownLabel>Settings</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem href="/privacy-policy">
+                  <ShieldCheckIcon />
+                  <DropdownLabel>Privacy policy</DropdownLabel>
+                </DropdownItem>
+                <DropdownItem href="/share-feedback">
+                  <LightBulbIcon />
+                  <DropdownLabel>Share feedback</DropdownLabel>
+                </DropdownItem>
+                <DropdownDivider />
+                <DropdownItem href="/logout">
+                  <ArrowRightStartOnRectangleIcon />
+                  <DropdownLabel>Sign out</DropdownLabel>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </SidebarFooter>
+        </Sidebar>
+      }
+    >
+      <main className="p-6">{children}</main>
+    </SidebarLayout>
+  )
+}
